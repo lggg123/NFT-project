646fc0ecce10fadcde875d9bbd931cee23a3ecf8
lggg123 <machinelearner2334@gmail.com>
2023-03-05 21:06:05 -0800

    changes

--- test/controllers/landings_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class LandingsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get landings_index_url
-    assert_response :success
-  end
-
-end
